b33cb0fdbb84455ee5144cf968b90120632d808f
Dylan-Odjidja <dylan.odjidja@mail.bcu.ac.uk>
2024-05-18 15:42:57 +0100

    Revert "Squashed commit of the following:"
    This reverts commit 380ecf4ad73b339f4ff6c5d1bca4763a58783e43.

--- Samurai-GameAudio/Assets/Scripts/PlayerWarp.cs
+++ Samurai-GameAudio/Assets/Scripts/PlayerWarp.cs
@@ -1,7 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerWarp : MonoBehaviour
+
+// This script warps the player to locations for testing purposes as is not designed to be used in a final game. 
+
 {
+    // Start is called before the first frame update
     public Transform warpMarker1;
     public Transform warpMarker2;
     public Transform warpMarker3;
@@ -10,45 +16,43 @@ public class PlayerWarp : MonoBehaviour
     public Transform warpMarker6;
 
     public Transform playerStartLocation;
+
+
     public Transform player;
 
     void Awake()
     {
         player.position = playerStartLocation.position;
-        player.rotation = playerStartLocation.rotation; 
+        player.rotation = playerStartLocation.rotation;
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             player.position = warpMarker1.position;
-            FMODUnity.RuntimeManager.PlayOneShot("event:/Player/Warp", this.transform.position);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             player.position = warpMarker2.position;
-            FMODUnity.RuntimeManager.PlayOneShot("event:/Player/Warp", this.transform.position);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             player.position = warpMarker3.position;
-            FMODUnity.RuntimeManager.PlayOneShot("event:/Player/Warp", this.transform.position);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             player.position = warpMarker4.position;
-            FMODUnity.RuntimeManager.PlayOneShot("event:/Player/Warp", this.transform.position);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha5))
         {
             player.position = warpMarker5.position;
-            FMODUnity.RuntimeManager.PlayOneShot("event:/Player/Warp", this.transform.position);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha6))
         {
             player.position = warpMarker6.position;
-            FMODUnity.RuntimeManager.PlayOneShot("event:/Player/Warp", this.transform.position);
         }
     }
 }
