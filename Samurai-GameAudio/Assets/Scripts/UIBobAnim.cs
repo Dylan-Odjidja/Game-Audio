06f576a2f55ad2cd35f1a8017d7ca2715f21e032
Dylan-Odjidja <dylan.odjidja@mail.bcu.ac.uk>
2024-05-18 15:31:47 +0100

    Squashed commit of the following:
    commit 9755fda09f1b5f05d1caba7eb73d8609c9debd21
    Author: imaanchx <imaan.choudhury@mail.bcu.ac.uk>
    Date:   Sat May 18 14:50:38 2024 +0100
    
        errors
    
    commit 464f1a966da15adb0a512a478e3808c226aeebb6
    Author: imaanchx <imaan.choudhury@mail.bcu.ac.uk>
    Date:   Fri May 17 22:27:13 2024 +0100
    
        grass
    
    commit aaacd35b17a34d77c9400f58ab7e93a58486333e
    Author: imaanchx <imaan.choudhury@mail.bcu.ac.uk>
    Date:   Fri May 17 16:05:42 2024 +0100
    
        g
    
    commit 9fd2bd61ec60487f942bd33f0e309c66bde18d71
    Author: imaanchx <imaan.choudhury@mail.bcu.ac.uk>
    Date:   Fri May 17 13:54:42 2024 +0100
    
        leek
    
    commit eebd81cdd340f8e7dd437386aa8a9847c87d586d
    Author: imaanchx <imaan.choudhury@mail.bcu.ac.uk>
    Date:   Fri May 17 13:20:02 2024 +0100
    
        menu
    
    commit 8b30d7a8b0db22b8df79c499a0f3415e33c83e1c
    Author: Dylan-Odjidja <dylan.odjidja@mail.bcu.ac.uk>
    Date:   Thu May 16 19:53:03 2024 +0100
    
        Squashed commit of the following:
    
        commit 7615eb07953934b56df5a3a521a90b98b562c1a8
        Author: Dylan-Odjidja <dylan.odjidja@mail.bcu.ac.uk>
        Date:   Thu May 16 19:51:50 2024 +0100
    
            fixed sword sounds
    
        commit 0c265739210ed0b26447c095fec699f7abae3bab
        Author: Dylan-Odjidja <dylan.odjidja@mail.bcu.ac.uk>
        Date:   Thu May 16 18:58:03 2024 +0100
    
            fixed quest sounds

--- Samurai-GameAudio/Assets/Scripts/UIBobAnim.cs
+++ Samurai-GameAudio/Assets/Scripts/UIBobAnim.cs
@@ -1,35 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class UIBobAnim : MonoBehaviour
 {
-    
-     float topDistance = 0.004f;
-   
-     float bottomDistance = -0.004f;
+    //public StudioEventEmitter popupSoundEmitter; // Assign the FMOD StudioEventEmitter component for the popup sound in the Inspector
 
+    float topDistance = 0.004f;
+    float bottomDistance = -0.004f;
     float t = 0.0f;
-
 
     void Update()
     {
+        this.transform.localPosition += new Vector3(0, Mathf.Lerp(bottomDistance, topDistance, t), 0);
 
-       // Debug.Log("t = "+t);
-    this.transform.localPosition += new Vector3(0,Mathf.Lerp(bottomDistance, topDistance,t),0);
+        t += 0.5f * Time.deltaTime;
 
-     t += 0.5f * Time.deltaTime;
-
-     if (t > 1.0f)
+        if (t > 1.0f)
         {
             float temp = topDistance;
             topDistance = bottomDistance;
             bottomDistance = temp;
             t = 0.0f;
+
+            // Play the popup sound when the menu pops up
+           // if (popupSoundEmitter != null)
+            //{
+               // popupSoundEmitter.Play();
+           // }
         }
-
-
-
-
     }
 }
